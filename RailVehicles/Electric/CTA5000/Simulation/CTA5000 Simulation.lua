38ab0b301b521e35a132592cf09d8c39b31b433d
Brian R <briman94@gmail.com>
2015-10-25 14:56:23 -0500

    Forgot something

--- RailVehicles/Electric/CTA5000/Simulation/CTA5000 Simulation.lua
+++ RailVehicles/Electric/CTA5000/Simulation/CTA5000 Simulation.lua
@@ -178,7 +178,7 @@ function Update(interval)
 				Call( "*:SetControlValue", "ThrottleLever", 0, CombinedLever )
 				
 				if (Call("*:GetControlValue", "ATCEnabled", 0) > 0) then
-					local restrictedSpeed = math.max(Call("*:GetControlValue", "ATCRestrictedSpeed", 0), 6)
+					local restrictedSpeed = math.max(Call("*:GetControlValue", "ATCRestrictedSpeed", 0), 2)
 					
 					if not gATOCoasting then
 						-- Begin ATO coast-override subsystem (Section 13.23 in 7000-series RFP)
